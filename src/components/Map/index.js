7ee0e9b666644f13314a04a5c11ff3e2befcce09
Serg <benderlio@gmail.com>
2023-02-11 15:29:59 +0100

    trips layer

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -3,8 +3,10 @@ import Map, { Source, Layer, NavigationControl } from 'react-map-gl';
 /// app.js
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { ScatterplotLayer, GeoJsonLayer } from '@deck.gl/layers/typed';
+import { TripsLayer } from '@deck.gl/geo-layers/typed';
 import { MapboxOverlay } from '@deck.gl/mapbox/typed';
 import { useControl } from 'react-map-gl';
+import { useState, useEffect } from 'react';
 
 function DeckGLOverlay(props) {
     const overlay = useControl(() => new MapboxOverlay(props));
@@ -15,6 +17,25 @@ function DeckGLOverlay(props) {
 const MapContainer = ({ geojsonData }) => {
 
     const TOKEN = 'pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pwY3owbGFxMDVwNTNxcXdwMms2OWtzbiJ9.1PPVl0VLUQgqrosrI2nUhg';
+
+
+    const [time, setTime] = useState(0);
+    const [animation] = useState({});
+
+
+    const animationSpeed = 1;
+    const loopLength = 1800;
+
+    const animate = () => {
+        setTime(t => (t + animationSpeed) % loopLength);
+        animation.id = window.requestAnimationFrame(animate);
+    };
+
+    useEffect(() => {
+        animation.id = window.requestAnimationFrame(animate);
+        return () => window.cancelAnimationFrame(animation.id);
+    }, [animation]);
+
 
     const geojson = geojsonData || {
         type: 'FeatureCollection',
@@ -75,6 +96,20 @@ const MapContainer = ({ geojsonData }) => {
         getElevation: 30
     });
 
+    const trips = new TripsLayer({
+        id: 'trips',
+        data: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/trips/trips-v7.json',
+        getPath: d => d.path,
+        getTimestamps: d => d.timestamps,
+        getColor: d => [253, 128, 93],
+        opacity: 0.3,
+        widthMinPixels: 2,
+        rounded: true,
+        trailLength: 180,
+        currentTime: time,
+        shadowEnabled: false
+    })
+
     return (
         <>
             <Map
@@ -90,7 +125,7 @@ const MapContainer = ({ geojsonData }) => {
                 interactiveLayerIds={['route']}
                 onClick={onClick}
             >
-                <DeckGLOverlay layers={[scatterplotLayer, layer]} />
+                <DeckGLOverlay layers={[scatterplotLayer, layer, trips]} />
                 <NavigationControl />
                 <Source id="my-data" type="geojson" data={geojson}>
                     <Layer {...layerStyle} />
